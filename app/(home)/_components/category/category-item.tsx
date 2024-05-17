79fae2c92004266c05cefc19999279a44d5453c8
felipekgouvea <felipekinupesg@hotmail.com>
2024-05-17 12:41:46 -0300

    feat: add categoriesIcon

--- app/(home)/_components/category/category-item.tsx
+++ app/(home)/_components/category/category-item.tsx
@@ -27,7 +27,7 @@ const CategoryItem = ({ category }: CategoryItamProps) => {
       variant="outline"
       className="flex items-center justify-center gap-2 rounded-[10px] border border-muted py-3"
     >
-      {categoryIcon[category.slug as keyof typeof categorieIcon]}
+      {categoryIcon[category.slug as keyof typeof categoryIcon]}
       <span className="text-xs font-bold">{category.name}</span>
     </Badge>
   )
---
+++ app/_helpers/categories-icons.ts
@@ -0,0 +1,10 @@
+// export const formatCurrency = (value: number): string => {
+//   const categoryIcon = {
+//     keyboards: <KeyboardIcon size={16} />,
+//     monitors: <MonitorIcon size={16} />,
+//     mousepads: <SquareIcon size={16} />,
+//     speakers: <SpeakerIcon size={16} />,
+//     mouses: <MouseIcon size={16} />,
+//     headphones: <HeadphonesIcon size={16} />,
+//   }
+// }
