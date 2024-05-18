def78096533693cd20ed7bdbfb49923789638dc5
felipekgouvea <felipekinupesg@hotmail.com>
2024-05-18 16:27:10 -0300

    add: skeleton and product skeleton components

---
+++ app/(home)/_components/product/product-deals-skeleton.tsx
@@ -0,0 +1,12 @@
+import { Skeleton } from '@/app/_components/ui/skeleton'
+
+const ProductDealsSkeleton = () => {
+  return (
+    <>
+      <Skeleton className="h-170 w-156" />
+      <Skeleton className="h-170 w-156" />
+    </>
+  )
+}
+
+export default ProductDealsSkeleton
